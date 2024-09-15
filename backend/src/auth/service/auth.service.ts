d141929647535a52b717bcfd3bde7c5d7a24320a
markpakingan <116990353+markpakingan@users.noreply.github.com>
2024-09-14 23:07:48 -0700

    fixed authentication with hashing

--- backend/src/auth/service/auth.service.ts
+++ backend/src/auth/service/auth.service.ts
@@ -4,16 +4,25 @@ import { Repository } from 'typeorm';
 import { LoginDto } from '../dto/LoginDto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
+    private readonly saltRounds = 10
+
     constructor(
         @InjectRepository(UserEntity)
         private readonly repo: Repository<UserEntity>,
-        private jwtService: JwtService
+        private jwtService: JwtService,
     ){}
 
+
+
+    async validatePassword(password:string, hashedPassword:string): Promise<boolean>{
+        return await bcrypt.compare(password, hashedPassword)
+    }
         
+
     async Login(loginDto: LoginDto): Promise<any>{
 
         const user = await this.repo.findOne({where: {username: loginDto.username}})
@@ -23,7 +32,9 @@ export class AuthService {
             throw new NotFoundException("Username not found!")
         }
 
-        if(user?.password !== loginDto.password){
+        const isPasswordValid = await this.validatePassword(loginDto.password, user.password)
+
+        if(!isPasswordValid){
             throw new UnauthorizedException("Incorrect password")
         }
 
